96aa9112ec4b2e66aa3611ed435e444c095f8545
Jan Fabry <jan.fabry@monkeyman.be>
2020-05-14 22:01:25 +0200

    Add CORS headers to editor
    Only there, so no library used.

--- facemask_shop/editor/views.py
+++ facemask_shop/editor/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse, HttpResponseNotAllowed
+from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from facemask_shop.editor.forms import FacemaskForm
@@ -6,11 +6,29 @@ from facemask_shop.editor.forms import FacemaskForm
 
 @csrf_exempt
 def upload_facemask(request):
+    if request.method == 'OPTIONS':
+        response = HttpResponse()
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Headers'] = ', '.join((
+            "accept",
+            "accept-encoding",
+            "authorization",
+            "content-type",
+            "dnt",
+            "origin",
+            "user-agent",
+            "x-csrftoken",
+            "x-requested-with",
+        ))
+        response['Access-Control-Allow-Methods'] = 'POST,OPTIONS'
+        return response
     if request.method == 'POST':
         form = FacemaskForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             facemask = form.save()
-            return JsonResponse({'success': True, 'mask_id': facemask.id})
+            response = JsonResponse({'success': True, 'mask_id': facemask.id})
         else:
-            return JsonResponse({'success': False, 'errors': form.errors}, status=400)
+            response = JsonResponse({'success': False, 'errors': form.errors}, status=400)
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
     return HttpResponseNotAllowed(permitted_methods=('POST',))
