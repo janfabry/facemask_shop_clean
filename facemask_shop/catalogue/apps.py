7c3062d822c43de307b2403b68f682e01113b373
Jan Fabry <jan.fabry@monkeyman.be>
2020-05-25 22:07:54 +0200

    Skip catalog view
    Homepage and products, and nothing in between!

--- facemask_shop/catalogue/apps.py
+++ facemask_shop/catalogue/apps.py
@@ -1,7 +1,20 @@
 import oscar.apps.catalogue.apps as apps
 from django.conf.urls import url
-from django.urls import include
+from django.urls import reverse_lazy
+from django.views.generic import RedirectView
 
 
 class CatalogueConfig(apps.CatalogueConfig):
     name = 'facemask_shop.catalogue'
+
+    def get_urls(self):
+        urls = [
+            url(r'^$', RedirectView.as_view(url=reverse_lazy('home')), name='index'),
+            url(r'^(?P<product_slug>[\w-]*)_(?P<pk>\d+)/$',
+                self.detail_view.as_view(), name='detail'),
+            url(r'^category/(?P<category_slug>[\w-]+(/[\w-]+)*)_(?P<pk>\d+)/$',
+                RedirectView.as_view(url=reverse_lazy('home')), name='category'),
+            url(r'^ranges/(?P<slug>[\w-]+)/$',
+                RedirectView.as_view(url=reverse_lazy('home')), name='range'),
+        ]
+        return self.post_process_urls(urls)
