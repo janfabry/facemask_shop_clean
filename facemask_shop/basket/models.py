1adecad3d624ae0f7d640555188089e78b34c866
Jan Fabry <jan.fabry@monkeyman.be>
2020-05-25 21:01:32 +0200

    Hide product options in basket

--- facemask_shop/basket/models.py
+++ facemask_shop/basket/models.py
@@ -1,1 +1,13 @@
+from django.utils.encoding import smart_text
+from oscar.apps.basket.abstract_models import AbstractLine
+
+
+class Line(AbstractLine):
+    @property
+    def description(self):
+        d = smart_text(self.product)
+        # Don't show mask image ID (could show other options later)
+        return d
+
+
 from oscar.apps.basket.models import *  # noqa isort:skip
